2544f8cb2296bb5fbbe03c46f00aa1bd6fcb4fc6
Hari <anarahari@gmail.com>
2017-06-16 03:56:39 +0800

    added logic to read all files and load to a database

--- constants.py
+++ constants.py
@@ -1,6 +1,9 @@
 FILENAME='data.json'
 TSTAMP='%Y-%m-%dT%H:%M:%S%z'
 DATE='%Y%m%d'
+TABLE_CREATE_QUERY='create table if not exists taxis (id integer primary key autoincrement,date text, time text,x integer, y integer,value integer)'
+TABLE_INSERT_QUERY='insert into taxis (date,time,x,y,value) values (%s,%s,%d,%d,%d)'
+
 
 #Singapore bounds as taken from Google maps - includes From Tuas to Changi and 
 #from Sentosa to Woodlands
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sqlite3
 from constants import *
 
 def read_json(filename):
@@ -9,8 +10,8 @@ def read_json(filename):
 def get_width(h):
 	return int(h * (max_lon - min_lon) / (max_lat - min_lat))
 
-def create_grid(h,w):
-	grid = [[0] * (h+1) for i in range(w+1)]
+def create_grid(w,h):
+	grid = [[0] * (w+1) for i in range(h+1)]
 	return grid
 
 def get_grid_cell(lat,lon,w,h):
@@ -33,6 +34,16 @@ def get_rankings(grid):
 	}
 	return rankings
 
+def file_to_grid(filename,grid_height=20):
+	grid_width=get_width(grid_height)
+	grid=create_grid(grid_width,grid_height)
+	data=read_json(filename)
+	for lon,lat in data['features'][0]['geometry']['coordinates']:
+		x,y = get_grid_cell(lat,lon,grid_width,grid_height)
+		grid[y][x]+=1
+	return grid
+
+
 def list_data_files(root):
 	result=[]
 	for root, dirs, files in os.walk(root):
@@ -40,3 +51,25 @@ def list_data_files(root):
 			date,time = root.split("/")[1:]
 			result.append((root+"/"+FILENAME,date,time))
 	return result
+
+def process_data(directory,grid_height=20):
+	result=[]
+	files = list_data_files(directory)
+	for filename, date, time in files:
+		grid=file_to_grid(filename,grid_height)
+		result.append((date,time,grid))
+	return result
+
+def load_to_db(dbname,directory,grid_height=20):
+	db=sqlite3.connect(dbname)
+	db.execute(TABLE_CREATE_QUERY)
+	data=process_data(directory,grid_height)
+	grid_width=get_width(grid_height)
+	for date, time, grid in data:
+		for i in range(grid_height+1):
+			for j in range(grid_width+1):
+				db.execute(TABLE_INSERT_QUERY%(date,time,i,j,grid[i][j]))
+	db.commit()
+	db.close()
+
+
