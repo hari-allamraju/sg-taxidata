0aab3af20892349609a07382d04a6afaa002bfd1
Hari <anarahari@gmail.com>
2017-06-25 12:09:29 +0800

    fixed issue with grid save to DB and tested fetch logic

--- taxis.py
+++ taxis.py
@@ -8,9 +8,9 @@ def load_taxi_to_db(dbname,directory,grid_height=20):
 	data=process_data(directory,TAXI_FILENAME,grid_height)
 	grid_width=get_width(grid_height)
 	for date, time, grid in data:
-		for i in range(grid_height+1):
-			for j in range(grid_width+1):
-				db.execute(TAXI_TABLE_INSERT_QUERY,(date,time,i,j,grid[i][j]))
+		for y in range(grid_height+1):
+			for x in range(grid_width+1):
+				db.execute(TAXI_TABLE_INSERT_QUERY,(date,time,x,y,grid[y][x]))
 	db.commit()
 	db.close()
 
@@ -23,13 +23,15 @@ def fetch_grid_by_time(dbname,time):
 def fetch_grid_by_date_and_time(dbname,date,time):
 	return fetch_from_db(dbname,TAXI_BY_DATE_AND_TIME_QUERY,(date,time))
 
-def fetch_loc_by_date(dbname,date,lat,lon):
+def fetch_loc_by_date(dbname,date,lat,lon,grid_height=20):
+	x,y=get_grid_cell(lat,lon,grid_height)
+	return fetch_from_db(dbname,TAXI_BY_DATE_QUERY_XY,(date,x,y))
 
-	return fetch_from_db(dbname,TAXI_BY_DATE_QUERY,(date,x,y))
+def fetch_loc_by_time(dbname,time,lat,lon,grid_height=20):
+	x,y=get_grid_cell(lat,lon,grid_height)
+	return fetch_from_db(dbname,TAXI_BY_TIME_QUERY_XY,(time,x,y))
 
-def fetch_loc_by_time(dbname,time):
-	return fetch_from_db(dbname,TAXI_BY_TIME_QUERY,(time,x,y))
-
-def fetch_loc_by_date_and_time(dbname,date,time):
-	return fetch_from_db(dbname,TAXI_BY_DATE_AND_TIME_QUERY,(date,time,x,y))
+def fetch_loc_by_date_and_time(dbname,date,time,lat,lon,grid_height=20):
+	x,y=get_grid_cell(lat,lon,grid_height)
+	return fetch_from_db(dbname,TAXI_BY_DATE_AND_TIME_QUERY_XY,(date,time,x,y))
 
