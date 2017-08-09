e97dda0f465493ed28a6e2f1d234cd53765dc3f8
Hari <anarahari@gmail.com>
2017-08-09 13:54:58 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sgtaxidata',
+      version='0.1',
+      description='Simple API for processing taxi data extracted using Singapore open data API',
+      url='https://github.com/hari-allamraju/sg-taxidata',
+      author='Hari Allamraju',
+      author_email='anarahari@gmail.com',
+      license='None',
+      packages=['sgtaxidata','sgtaxidata.examples'],
+      zip_safe=False)
